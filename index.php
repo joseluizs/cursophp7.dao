6f83978125e4bcbafc00ce994be2d5d4dbc29392
joseluizs <36302287+joseluizs@users.noreply.github.com>
2018-09-25 15:20:54 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,11 +2,39 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+// Antes fazia assim
+//$sql = new Sql();
 
-$usuarios = $sql->select("select * from tb_usuarios ");
+//$usuarios = $sql->select("select * from tb_usuarios ");
 
-echo json_encode($usuarios);
+//echo json_encode($usuarios);
+
+//Agora faz desse jeito
+
+/*Carrega apenas um usuario
+$variavel = new Usuario();
+$variavel ->loadById(1);
+echo $variavel;
+*/
+
+/*
+// Carrega uma lista de usuarios
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+/*
+//carrega uma lista de usuarios pelo login
+$search = Usuario::search("jo");
+
+echo json_encode($search);
+*/
+
+//Carrega o usuario usando o login e senha
+$usuario = new Usuario();
+$usuario->login("jose", "123");
+echo $usuario;
 
 
 ?>
