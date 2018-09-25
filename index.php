6b19e3831fce1c759cf6d3951cf915c672692d2e
joseluizs <joseluiz.smarques@gmail.com>
2018-09-25 15:10:11 -0300

    Tentando enviar do pc

--- index.php
+++ index.php
@@ -11,12 +11,30 @@ require_once("config.php");
 
 //Agora faz desse jeito
 
+/*Carrega apenas um usuario
 $variavel = new Usuario();
-
 $variavel ->loadById(1);
-
 echo $variavel;
+*/
 
+/*
+// Carrega uma lista de usuarios
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+/*
+//carrega uma lista de usuarios pelo login
+$search = Usuario::search("jo");
+
+echo json_encode($search);
+*/
+
+//Carrega o usuario usando o login e senha
+$usuario = new Usuario();
+$usuario->login("jose", "123");
+echo $usuario;
 
 
 ?>
