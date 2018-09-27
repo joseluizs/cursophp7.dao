a91921c32e01e64f087e93aa449fcaa279b7ba9d
joseluizs <36302287+joseluizs@users.noreply.github.com>
2018-09-27 16:02:32 -0300

    Updade 5.0

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class Usuario{
@@ -50,12 +49,8 @@ class Usuario{
 		
 		));
 		if(count($results) >0){
-			$row = $results[0];
 			
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime ($row['dtcadastro']));
+			$this->setData($results[0]);
 		
 		}
 	
@@ -89,17 +84,40 @@ class Usuario{
 		
 		));
 		if(count($results) >0){
-			$row = $results[0];
-			
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime ($row['dtcadastro']));
+			$this->setData($results[0]);			
 		
 		}else{
 		
 			throw new Exception("Login e/ou senha inválidos !");
 		}
+	
+	}
+	
+	public function insert(){
+		$sql = new Sql();
+		
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha()
+		));
+		if(count($results) > 0){
+			$this->setData($results[0]);
+		}
+	}
+	
+	public function __construct($login = "", $password = ""){
+		
+	$this->setDeslogin($login);
+	$this->setDessenha($password);
+		
+	}
+	
+	public function setData($data){
+		
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime ($data['dtcadastro']));
 	
 	}
 	
@@ -111,12 +129,7 @@ class Usuario{
 			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 		
 		));
-	
-	
 	}
-
-
-
 }
 
 
