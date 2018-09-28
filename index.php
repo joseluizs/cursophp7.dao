bf96d774ffaab5a7ac82d6ca27455fd8c1f9aea2
joseluizs <joseluiz.smarques@gmail.com>
2018-09-28 10:07:57 -0300

    Agora fizemos o comando delete

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require_once("config.php");
 //$usuarios = $sql->select("select * from tb_usuarios ");
 
 //echo json_encode($usuarios);
-<<<<<<< HEAD
 
 //Agora faz desse jeito
 
@@ -54,53 +53,17 @@ $aluno1->insert();
 echo $aluno1;
 */
 
+/* //Alterar o usuario
 $usuario = new Usuario();
 $usuario->loadById(2);
 $usuario->update("professor", "!@#$%*");
 
 echo $usuario;
-=======
->>>>>>> 63b6a8e8ea212a81263e15a54b90d275410182f6
-
-//Agora faz desse jeito
-
-/*Carrega apenas um usuario
-$variavel = new Usuario();
-$variavel ->loadById(1);
-echo $variavel;
 */
-
-/*
-// Carrega uma lista de usuarios
-$lista = Usuario::getList();
-
-echo json_encode($lista);
-*/
-
-/*
-//carrega uma lista de usuarios pelo login
-$search = Usuario::search("jo");
-
-echo json_encode($search);
-*/
-
-/*
-//Carrega o usuario usando o login e senha
 $usuario = new Usuario();
-$usuario->login("jose", "123");
+$usuario->loadById(2);
+$usuario->delete();
 echo $usuario;
-*/
-
-/*
-//INSERE NO BANCO COM PROCEDURE E RETORNA OS VALORES
-$aluno = new Usuario();
-$aluno->setDeslogin("aluno");
-$aluno->setDessenha("@luno");
-$aluno->insert();
-echo $aluno;
-*/
-$aluno1 = new Usuario("luizinho","@luno1");
-$aluno1->insert();
-echo $aluno1;
+	
 
 ?>
